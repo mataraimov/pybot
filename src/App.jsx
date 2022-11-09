be8305829dd8b5a06aaa7cf48dc31226848cd8a6
mataraimov <73570575+mataraimov@users.noreply.github.com>
2022-11-09 13:51:18 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
 
     return (
         <div className="App">
+            <h1>Work</h1>
             <Header />
             <Routes>
                 <Route index element={<ProductList />}/>
